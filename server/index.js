231806020a3cdc6928eeced31fcb6b5fca360c39
Jinko07 <golybev0912@mail.ru>
2023-05-11 19:15:58 +0300

    fix error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-require('.env').config();
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const PORT = express.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const start = async() => {
     try{
